a31154ce871e7440100b38edc3635e174565c644
Ondrej2024 <ondra.klein20@seznam.cz>
2024-08-12 18:30:08 +0200

    změna loga Z5

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -7,7 +7,7 @@ export const Logo: FC<Props> = () => {
 	return (
 		<div className="flex flex-row items-center gap-2">
 			<h1 className="text-3xl font-bold">HAJANSKÝ</h1>
-			<Image src="/logo2.png" alt="Logo" width={150} height={150} />
+			<Image src="/logo.png" alt="Logo" width={150} height={150} />
 			<h1 className="text-3xl font-bold">FOTOGRAF</h1>
 		</div>
 	);
